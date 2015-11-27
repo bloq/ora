aaa93c64e2c47476cb3d5359cc55a19ef3f21015
Jeff Garzik <jeff@bloq.com>
2015-11-27 13:17:10 -0500

    oracli: remove unneeded #includes

--- src/oracli.cc
+++ src/oracli.cc
@@ -15,24 +15,11 @@
 #include <string>
 #include <assert.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <argp.h>
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-
-#include <event2/bufferevent_ssl.h>
-#include <event2/bufferevent.h>
-#include <event2/buffer.h>
-#include <event2/listener.h>
-#include <event2/util.h>
-#include <event2/http.h>
-
-#include <openssl/ssl.h>
-#include <openssl/err.h>
-#include <openssl/rand.h>
+#include <event2/event.h>
 
 #include "oraapi.h"
 
