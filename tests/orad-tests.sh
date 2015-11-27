819114907d7369fa7da80240a1a63d1323ea9223
Jeff Garzik <jeff@bloq.com>
2015-11-27 15:19:55 -0500

    Add orad startup/shutdown test, and skeleton for further tests.

---
+++ tests/orad-tests.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+CWD=`pwd`
+PID_FN="$CWD/orad.pid"
+
+#
+# Daemon startup
+#
+
+# PID file cannot be present at start of tests
+if [ -f $PID_FN ]
+then
+	echo Daemon PID file already exists.
+	exit 1
+fi
+
+# Start daemon in background
+../src/orad -p $PID_FN &
+sleep 1
+
+# PID file must exist
+if [ ! -f $PID_FN ]
+then
+	echo Daemon not started.
+	exit 1
+fi
+
+# Ping process to make sure it exists
+kill -0 `cat $PID_FN`
+if [ $? -ne 0 ]
+then
+	echo "Daemon cannot be pinged with kill(1)"
+	kill %1
+	rm -f $PID_FN
+	exit 1
+fi
+
+# TODO
+
+#
+# Daemon shutdown
+#
+
+if [ ! -f $PID_FN ]
+then
+	echo Daemon PID file is missing.
+	exit 1
+fi
+
+kill `cat $PID_FN`
+if [ $? -ne 0 ]
+then
+	echo "Daemon cannot be reached via kill(1)"
+	exit 1
+fi
+
+sleep 1
+
+if [ -f $PID_FN ]
+then
+	echo Daemon PID file remains.
+	exit 1
+fi
+
+exit 0
